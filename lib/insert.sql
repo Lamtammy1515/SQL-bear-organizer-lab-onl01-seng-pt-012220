8e75d6789f8abe1ed3ee08db39a0efff0401519c
Tammy Lam <lamtammy1515@gmail.com>
2020-03-17 05:20:35 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
